ccd99d7de3acc814500f53c89254bd19995195e6
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-15 09:30:41 +0800

    build: remove unused xpip-build dependency from pyproject.toml

--- xkits_logger/attribute.py
+++ xkits_logger/attribute.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 
-__version__ = "0.3"
-
 # project info
 __project_name__ = "xlogger"
 __project_home__ = "https://github.com/bondbox/xlogger/"
 __project_desc__ = "Logging module"
+
+# package info
+__version__ = "0.3"
+__package_name__ = "xkits-logger"
+__package_desc__ = "Logging module"
